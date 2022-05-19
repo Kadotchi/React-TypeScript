af42b57d41e5615228be2538cfd16c9c034eb191
Kadotchi <84917429+Kadotchi@users.noreply.github.com>
2022-05-20 00:07:02 +0900

    コンポーネント自体の型定義

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,12 +1,8 @@
+import { VFC } from "react";
 import { TodoType } from "./Types/todo";
 
 //propsに型を定義
-export const Todo = (
-  //Pickの場合は選択
-  // props: Pick<TodoType, "userId" | "title" | "userId" | "completed">
-  //Omitの場合は省略
-  props: Omit<TodoType, "id">
-) => {
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
   const { title, userId, completed = false } = props;
   const completedMark = completed ? "【完】" : "【未】";
   return <p>{`${completedMark}${title}(ユーザー；${userId})`}</p>;
