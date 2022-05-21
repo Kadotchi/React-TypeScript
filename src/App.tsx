5472127546ab6b3af914d5f8d381eb36936bbc6b
Kadotchi <84917429+Kadotchi@users.noreply.github.com>
2022-05-21 23:09:43 +0900

    オプションナルチェイニングでnull安全なコードを書く

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./Types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./Types/user";
+
+const user: User = {
+  name: "かどっち"
+  // hobbies: ["映画", "ゲーム"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,6 +24,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
