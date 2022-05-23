79aed3005cf0d0e3864f3e74c3c77012f870e3e1
Kadotchi <84917429+Kadotchi@users.noreply.github.com>
2022-05-23 23:30:51 +0900

    カスタムフックを使わないデータ取得例：userCardの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,17 @@
-import axios from "axios";
-import { useState } from "react";
-
 import "./styles.css";
-import { Todo } from "./Todo";
-import { TodoType } from "./Types/todo";
-import { Text } from "./Text";
-import { UserProfile } from "./UserProfile";
-import { User } from "./Types/user";
+import { UserCard } from "./components/UserCard";
 
-const user: User = {
-  name: "かどっち"
-  // hobbies: ["映画", "ゲーム"]
+const user = {
+  id: 1,
+  name: "かどっち",
+  email: "12345@aaa.com",
+  address: "ABCDEFG"
 };
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<TodoType>>([]);
-  const onClickFetchData = () => {
-    axios
-      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => {
-        setTodos(res.data);
-      });
-  };
   return (
     <div className="App">
-      <UserProfile user={user} />
-      <Text color="red" fontSize="18px" />
-      <button onClick={onClickFetchData}>データ取得</button>
-      {todos.map((todo) => (
-        <Todo
-          title={todo.title}
-          userId={todo.userId}
-          completed={todo.completed}
-        />
-      ))}
+      <UserCard user={user} />
     </div>
   );
 }
